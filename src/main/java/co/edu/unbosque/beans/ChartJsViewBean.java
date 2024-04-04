8e48bc3d68fbb3c60e4c8592a8368f5457a9cd66
Akalienjoyer <watchfullfox@gmail.com>
2024-04-04 01:57:24 -0500

    wwwwwwwwwwwwwwwwww4

---
+++ src/main/java/co/edu/unbosque/beans/ChartJsViewBean.java
@@ -0,0 +1,104 @@
+package co.edu.unbosque.beans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.primefaces.event.ItemSelectEvent;
+import org.primefaces.model.charts.ChartData;
+import org.primefaces.model.charts.pie.PieChartDataSet;
+import org.primefaces.model.charts.pie.PieChartModel;
+
+import co.edu.unbosque.model.User;
+import co.edu.unbosque.model.persistence.UserDAO;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+
+@Named("chartJsViewBean")
+@RequestScoped
+public class ChartJsViewBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private PieChartModel pieModel;
+	private UserDAO userDAO;
+
+	@PostConstruct
+	public void init() {
+		createPieModel();
+		
+	}
+
+	private void createPieModel() {
+		pieModel = new PieChartModel();
+		ChartData data = new ChartData();
+		userDAO = new UserDAO();
+		PieChartDataSet dataSet = new PieChartDataSet();
+		List<Number> values = new ArrayList<>();
+		for (int i = 0; i < 15; i++) {
+			int count = 0;
+			for (User obj : userDAO.readAll()) {
+				if (obj.getSemester() == (i+1)) {
+					count++;
+				}
+			}
+			values.add(count);
+		}
+		dataSet.setData(values);
+
+		List<String> bgColors = new ArrayList<>();
+		bgColors.add("rgb(255, 99, 132)");
+		bgColors.add("rgb(54, 162, 235)");
+		bgColors.add("rgb(255, 205, 86)");
+		bgColors.add("rgb(34, 99, 132)");
+		bgColors.add("rgb(88, 162, 235)");
+		bgColors.add("rgb(99, 205, 86)");
+		bgColors.add("rgb(12, 99, 132)");
+		bgColors.add("rgb(45, 162, 235)");
+		bgColors.add("rgb(67, 205, 86)");
+		bgColors.add("rgb(33, 99, 132)");
+		bgColors.add("rgb(22, 162, 235)");
+		bgColors.add("rgb(71, 205, 86)");
+		dataSet.setBackgroundColor(bgColors);
+
+		data.addChartDataSet(dataSet);
+		List<String> labels = new ArrayList<>();
+		labels.add("1");
+		labels.add("2");
+		labels.add("3");
+		labels.add("4");
+		labels.add("5");
+		labels.add("6");
+		labels.add("7");
+		labels.add("8");
+		labels.add("9");
+		labels.add("10");
+		labels.add("11");
+		labels.add("12");
+		labels.add("13");
+		labels.add("14");
+		labels.add("15");
+		data.setLabels(labels);
+
+		pieModel.setData(data);
+	}
+
+	public void itemSelect(ItemSelectEvent event) {
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",
+				"Item Index: " + event.getItemIndex() + ", DataSet Index:" + event.getDataSetIndex());
+
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public PieChartModel getPieModel() {
+		return pieModel;
+	}
+
+	public void setPieModel(PieChartModel pieModel) {
+		this.pieModel = pieModel;
+	}
+
+}
